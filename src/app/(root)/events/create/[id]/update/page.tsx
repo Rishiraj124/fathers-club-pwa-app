c8b447bf423a1bfcf650a861cd6aae8e94e13e8e
RishiDevGit <rishirajsaxena@makewebbetter.com>
2024-03-11 19:45:17 +0530

    main page route creation

---
+++ src/app/(root)/events/create/[id]/update/page.tsx
@@ -0,0 +1,13 @@
+const UpdateEvent = () => {
+  return (
+    <>
+      <section className='bg-primary-50 bg-dotted-pattern bg-cover bg-center py- md:py-10'>
+        <h3 className='wrapper h3-bold text-center sm:text-left'>
+          Create Event
+        </h3>{' '}
+      </section>
+      <div className='wrapper my-8'></div>
+    </>
+  );
+};
+export default UpdateEvent;
